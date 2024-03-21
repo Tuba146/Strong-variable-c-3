351e887f70f2a1d021e577a45771544cd3a1fa6c
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-03-21 13:58:57 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+//srong variable , type inference
+let userName = "Tuba Asif"; //string
+console.log(userName);
+let age = 24; //number
+console.log(age);
+let isStudent = false; //boolean
+console.log(isStudent);
+let randomvalue = "http://www.linkedin.com/in/Tuba ASif/"; //any
+console.log(randomvalue);
+//strongly type snytax
+let a = "pakistan";
+console.log(a);
+let b = 5;
+console.log(b);
+let c = true;
+console.log(c);
+//type inference 
+let d = "karachi";
+let e = 19;
+let f = false;
---
+++ userName.ts
@@ -0,0 +1,23 @@
+let userName: string = "Tuba Asif"; //string
+
+console.log(userName);
+let age = 24; //number
+
+console.log(age);
+let isStudent: boolean = false; //boolean
+
+console.log(isStudent);
+let randomvalue: any = "http://www.linkedin.com/in/Tuba ASif/"; //any
+
+console.log(randomvalue);
+//strongly type snytax
+let a: string = "pakistan";
+console.log(a);
+let b: number = 5;
+console.log(b);
+let c: boolean = true;
+console.log(c);
+//type inference 
+let d = "karachi";
+let e = 19;
+let f = false;
